6bef8e539c71cb4bbdc99675e472105763fcb4b8
Yann Hourdel <yann@hourdel.fr>
2018-08-02 20:28:47 +0200

    Separate jQuery-mosaico and Angular-mosaico

--- vendor/assets/javascripts/angular-mosaico.js
+++ vendor/assets/javascripts/angular-mosaico.js
@@ -1,35 +1,5 @@
 'use strict';
 
-var promises = {};
-var nextPromiseId = 0;
-
-// jQuery helper to communicate with mosaico frame
-$.fn.mosaico = function(action, datas) {
-  if (!datas) {
-    datas = {};
-  }
-  datas.action = action;
-
-  // create a promise
-  var deferred = $.Deferred();
-
-  // choose an ID for it
-  var promiseId = 0 + nextPromiseId;
-  nextPromiseId++;
-
-  // store the ID in the message data so that we get it in response
-  datas.promiseId = promiseId;
-
-  // create the promise and store it in promises
-  promises[promiseId] = deferred;
-
-  // send the message
-  this.get(0).contentWindow.postMessage(JSON.stringify(datas), '*');
-
-  // return callback-only object for the promise
-  return deferred.promise();
-};
-
 angular.module('angular-mosaico', [])
   .controller('MosaicoController', ['$scope', function($scope) {
     var rcvmsg = function(evt) {
